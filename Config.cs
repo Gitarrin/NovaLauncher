ea7f854c75b6e19d03d2c4d61823dd337b555445
Terminal <79934414+TermOfficial@users.noreply.github.com>
2025-03-21 18:50:41 -0400

    Revert changes

--- Config.cs
+++ Config.cs
@@ -9,18 +9,9 @@ namespace NovaLauncher
     {
         public static string baseUrl = "http://n.termy.lol/client/setup";
         public static string downloadCompleteUrl = "https://n.termy.lol/app/downloaded";
-#if CLIENT
-        public static string client = CLIENT;
-        public static string RevName = "Novarin "+CLIENT;
-#else
         public static string client = "2016";
         public static string RevName = "Novarin 2016";
-#endif
-#if PROTOCOL
-        public static string Protocol = PROTOCOL;
-#else
         public static string Protocol = "novarin16";
-#endif
         public static string installPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Novarizz\\" + client;
         public static bool doSha256Check = false;
     }
