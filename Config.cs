93696a8a9a097a312368d420873d07f7155f8c02
Loganh4005 <60761520+Preloading@users.noreply.github.com>
2025-03-26 20:35:59 -0600

    final build!

--- Config.cs
+++ Config.cs
@@ -9,9 +9,9 @@ namespace NovaLauncher
     {
         public static string baseUrl = "http://n.termy.lol/client/setup";
         public static string downloadCompleteUrl = "https://n.termy.lol/app/downloaded";
-        public static string client = "2016";
-        public static string RevName = "Novarin 2016";
-        public static string Protocol = "novarin16";
+        public static string client = "2012";
+        public static string RevName = "Novarin 2012";
+        public static string Protocol = "novarin12";
         public static string installPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Novarizz\\" + client;
         public static bool doSha256Check = false;
     }
