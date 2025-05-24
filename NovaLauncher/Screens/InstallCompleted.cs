8badd5389670abe77021b61a9b1d54fc9a783ce2
TheCodingGuy <git@nekos.sh>
2025-05-24 06:52:39 -0400

    tf was that formatting

--- NovaLauncher/Screens/InstallCompleted.cs
+++ NovaLauncher/Screens/InstallCompleted.cs
@@ -3,16 +3,16 @@ using System.Windows.Forms;
 
 namespace NovaLauncher.Screens
 {
-    public partial class InstallCompleted: UserControl
-    {
-        public InstallCompleted()
-        {
-            InitializeComponent();
-        }
+	public partial class InstallCompleted: UserControl
+	{
+		public InstallCompleted()
+		{
+			InitializeComponent();
+		}
 
-        public void CloseButton_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-    }
+		public void CloseButton_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
 }
