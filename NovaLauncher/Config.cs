607be71674d8657b6f2f8630f7965f7eb11c849e
parakeet <79934414+parakeet-live@users.noreply.github.com>
2026-01-14 18:22:42 -0500

    Update Config.cs

--- NovaLauncher/Config.cs
+++ NovaLauncher/Config.cs
@@ -6,15 +6,13 @@ namespace NovaLauncher
 	{
 		public static readonly string[] Servers = new string[]
 		{
-			"http://n.termy.lol", // Main
-			"http://wowlauncher.novarin.co", // Mirror
-			"http://termy.nekos.sh" // Mirror backup
+			"http://dl.novarin.cc"
 		};
 		public static string SelectedServer = "";
 		public static string LauncherSetup = "/client/launcher/";
 
-		public static string AppShortName = "Novarin";
-		public static string AppName = $"{AppShortName} Launcher";
+		public static string AppShortName = "Novarin (test)";
+		public static string AppName = $"{AppShortName} Launcher (test)";
 		public static string AppEXE = "NovaLauncher.exe";
 		public static string AppProtocol = AppShortName.ToLower();
 		public static bool Debug = false;
